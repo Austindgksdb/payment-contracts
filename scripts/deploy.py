ef1327e9760bdad20de650b570384493839d6af8
iamdefinitelyahuman <ben@hauser.id>
2022-10-24 18:42:56 +0400

    feat: deploy / basic test script

---
+++ scripts/deploy.py
@@ -0,0 +1,29 @@
+from brownie import Factory, MinimalProxy, Sweeper, accounts
+from brownie_tokens import ERC20
+
+
+def main():
+    deployer = accounts[0]
+
+    proxy_imp = MinimalProxy.deploy(deployer.get_deployment_address(deployer.nonce+1), {'from': deployer})
+    factory = Factory.deploy(deployer, proxy_imp, {'from': deployer})
+    sweeper_imp = Sweeper.deploy(factory, {'from': deployer})
+    factory.set_sweeper_implementation(sweeper_imp, {'from': deployer})
+
+
+def send_token_test():
+    main()
+
+    deployer = accounts[0]
+    alice = accounts[1]
+
+    token = ERC20()
+    factory = Factory[0]
+
+    factory.set_token_approvals([token], True, {'from': deployer})
+    factory.create_payment_address({'from': alice})
+    sweeper = Sweeper.at(factory.account_to_payment_address(alice))
+
+    token._mint_for_testing(alice, 10**18)
+    token.approve(sweeper, 2**256-1, {'from': alice})
+    sweeper.send_token(token, 10**17, {'from': alice})
